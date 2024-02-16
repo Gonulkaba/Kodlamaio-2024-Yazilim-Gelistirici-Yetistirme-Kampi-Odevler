3dbf18e25e642ccde8f926a28cf770d2dcebf8a0
Gönül Kaba <gonul.kaba123@gmail.com>
2024-02-17 00:34:30 +0300

    Hatfa1 Ödev2- Maske Takip ödevi tamamlandı.

---
+++ Hafta1-Odev2-MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tc No bilgisi MERNİS'ten kontrol edilerek, kişiye maske verilip verilemeyeceği kontrolü yapılıyor... \n");
+
+            Person person= new Person();
+            person.FirstName = "Gönül";
+            person.LastName = "Kaba";
+            person.DateOfBirthYear = 1999;
+            person.NationalIdentity = 123;
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person);
+        }
+    }
+}
+
